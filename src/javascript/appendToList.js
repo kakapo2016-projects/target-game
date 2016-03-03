1c949156e8999ef9912b28c447cd03c24ed54291
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-03-04 12:03:54 +1300

    started working on knex and also added an alert if isValid returns false

--- src/javascript/appendToList.js
+++ src/javascript/appendToList.js
@@ -1,7 +1,12 @@
 var request = require('superagent')
 var $ = require('jquery')
 
+var score = 0
+
 module.exports = 
 function appendToList(newWord){
   	$('#word-list').append('<li>' + newWord + '</li>')
+  	score ++
+  	console.log('score', score)
   }
+  //remember to append score into H3 tag. 
